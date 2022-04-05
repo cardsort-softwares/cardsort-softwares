d7ca5458fcc3fc8c508658d56ef1f922504494ac
MasterSaberD1996 <d.mcpherson.home@gmail.com>
2022-04-04 20:09:24 -0600

    update projects to run both builds and deploys

--- apps/website/src/app/app.module.ts
+++ apps/website/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
+import {FirebaseServicesModule} from "@cardsort-softwares/firebase-services";
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
+    FirebaseServicesModule
   ],
   providers: [],
   bootstrap: [AppComponent],
