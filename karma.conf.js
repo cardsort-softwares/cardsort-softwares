d16d1a25db3b30ba2023daa30c851ae616f2993d
MasterSaberD1996 <d.mcpherson.home@gmail.com>
2022-04-04 18:57:39 -0600

    fix closing brace

--- karma.conf.js
+++ karma.conf.js
@@ -38,7 +38,7 @@ module.exports = () => {
     colors: true,
     logLevel: constants.LOG_INFO,
     autoWatch: false,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
     singleRun: true,
   };
 };
