8335f38aa35a5223f17d81e40757790f10a988b8
MasterSaberD1996 <d.mcpherson.home@gmail.com>
2022-04-06 17:06:40 -0600

    fix tests

--- libs/website-core-ui/src/lib/header/header.component.spec.ts
+++ libs/website-core-ui/src/lib/header/header.component.spec.ts
@@ -1,14 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import {ImageService} from "@cardsort-softwares/firebase-services";
+import {AutoMocker} from "@cardsort-softwares/auto-mocker";
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let imageServiceMock: ImageService;
+  const autoMocker = new AutoMocker();
 
   beforeEach(async () => {
+    imageServiceMock = autoMocker.mockClass(ImageService);
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [ HeaderComponent ],
+      providers: [{
+        provide: ImageService,
+        useValue: imageServiceMock
+      }]
     })
     .compileComponents();
   });
@@ -16,10 +25,11 @@ describe('HeaderComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    autoMocker.withReturnObservable(imageServiceMock.getImage$, null);
+    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
 });
